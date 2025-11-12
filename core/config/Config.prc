660418c2398060ea1901728c8ff3096865114ec3
J0y-code <npflieger.chakma@gmail.com>
2025-11-12 01:14:17 +0100

    Delete core directory

--- core/config/Config.prc
+++
@@ -1,4 +0,0 @@
-window-title editor
-win-size 1600 940
-show-frame-rate-meter 1
-sync-video 0
--- core/ui/toolbar/__init__.py
+++
@@ -1,3 +0,0 @@
-from .transform_toolbar import TransformToolbar
-from .base_toolbar import BaseToolbar
-from .tool_button import ToolButton
